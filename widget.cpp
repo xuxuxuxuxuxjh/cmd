eb1c892b0635f6a6efd4c4d5a6a33bc7fcbf5a6a
xuxuxuxuxuxjh <48542487+xuxuxuxuxuxjh@users.noreply.github.com>
2022-10-20 11:27:10 +0800

    Add files via upload

---
+++ widget.cpp
@@ -0,0 +1,46 @@
+#include "widget.h"
+#include "ui_widget.h"
+
+Widget::Widget(QWidget *parent)
+    : QWidget(parent)
+    , ui(new Ui::Widget)
+{
+    ui->setupUi(this);
+
+    //连接型号和槽，  谁发出信号 发出什么信号 谁处理信号 怎么处理
+    connect(ui->cmdLineEdit,SIGNAL(returnPressed()),this,SLOT(on_commitButton_clicked()));
+
+    connect(ui->cancelButton,&QPushButton::clicked,[this]()
+    {
+        this->close();
+    });
+
+    connect(ui->browseButton,&QPushButton::clicked,[this]()
+    {
+        QMessageBox::information(this,"信息","点击浏览");
+    });
+
+}
+
+Widget::~Widget()
+{
+    delete ui;
+}
+
+
+void Widget::on_commitButton_clicked()
+{
+    //获取lineedit数据
+    QString program = ui->cmdLineEdit->text();
+
+
+    QProcess * myProcess = new QProcess(this);
+    myProcess->start(program);
+}
+
+
+void Widget::on_cancelButton_clicked()
+{
+    this->close();
+}
+
